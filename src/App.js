4844970110a355a2a31557049ee5f8103e71dec9
Sergio <norgestream@gmail.com>
2024-05-04 10:40:25 +0300

    use another way to create routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider, createRoutesFromElements, Route, createBrowserRouter } from 'react-router-dom';
 import HomePage from './components/Home';
+import ProductsPage from './components/Products';
 
-const router = createBrowserRouter([
-  {path: '', element: <HomePage />}
-]);
+const routesDefinition = createRoutesFromElements(
+  <Route>
+    <Route path= '' element= {<HomePage />}/>
+    <Route path= '/products' element= {<ProductsPage />}/>
+  </Route>
+);
+
+// const router = createBrowserRouter([
+//   {path: '', element: <HomePage />},
+//   {path: '/products', element: <ProductsPage />}
+// ]);
+
+const router = createBrowserRouter(routesDefinition)
 
 function App() {
   return <RouterProvider router={router}/>;
