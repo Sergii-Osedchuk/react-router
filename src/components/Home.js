655b9f6d2aac80cf279e448c0ec8ec17abd7edba
Sergio <norgestream@gmail.com>
2024-05-04 10:55:56 +0300

    use Link for navigating

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,10 @@
+import { Link } from 'react-router-dom';
+
 export default function HomePage () {
-  return (<h1>My Home Page</h1>)
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>List of products.<Link to='/products'>Click here</Link></p>
+    </>
+  );
 }
