e3147a1fc169f8176a6525d0f4b20950da6ee4d1
Sergio <norgestream@gmail.com>
2024-05-06 15:33:43 +0300

    use nested navigation and create error page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import { RouterProvider, createRoutesFromElements, Route, createBrowserRouter } from 'react-router-dom';
-import HomePage from './components/Home';
-import ProductsPage from './components/Products';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
+import ErrorPage from './pages/Error';
 
-const routesDefinition = createRoutesFromElements(
-  <Route>
-    <Route path= '' element= {<HomePage />}/>
-    <Route path= '/products' element= {<ProductsPage />}/>
-  </Route>
-);
+// const routesDefinition = createRoutesFromElements(
+//   // <Routes>
+//   //   <Route path='/' element={<RootLayout />} >
+//   //     <Route path= '' element= {<HomePage />}/>
+//   //     <Route path= '/products' element= {<ProductsPage />}/>
+//   //   </Route>
+//   // </Routes>
+// );
 
-// const router = createBrowserRouter([
-//   {path: '', element: <HomePage />},
-//   {path: '/products', element: <ProductsPage />}
-// ]);
+const router = createBrowserRouter([
+  {
+    path: '/', 
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {path: '', element: <HomePage />},
+      {path: '/products', element: <ProductsPage />},
+    ]
+  },
+]);
 
-const router = createBrowserRouter(routesDefinition)
 
 function App() {
   return <RouterProvider router={router}/>;
